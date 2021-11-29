bbfc31e9a4814d44693b7f0bd8376dc05b6f6a95
kyw <47565280+free-ko@users.noreply.github.com>
2021-11-30 08:24:23 +0900

    [FE] update : view class 개발

--- src/core/view.ts
+++ src/core/view.ts
@@ -1,1 +1,45 @@
-export default abstract class View {}
+export default abstract class View {
+  private container: HTMLElement;
+  private template: string;
+  private renderTemplate: string;
+  private htmlList: string[];
+
+  constructor(containerId: string, template: string) {
+    const containerElement: HTMLElement | null =
+      document.getElementById(containerId);
+
+    if (!containerElement) {
+      throw "최상위 컨테이너가 없어, UI Render를 진행하지 못합니다.";
+    }
+
+    this.container = containerElement;
+    this.template = template;
+    this.renderTemplate = template;
+    this.htmlList = [];
+  }
+
+  protected updateView(): void {
+    this.container.innerHTML = this.renderTemplate;
+    this.renderTemplate = this.template;
+  }
+
+  protected addHtml(htmlString: string): void {
+    this.htmlList.push(htmlString);
+  }
+
+  protected getHtml(): string {
+    const snapshot = this.htmlList.join("");
+    this.clearHtmlList();
+    return snapshot;
+  }
+
+  private clearHtmlList(): void {
+    this.htmlList = [];
+  }
+
+  protected setTemplateData(key: string, value: string) {
+    this.renderTemplate = this.renderTemplate.replace(`{{__${key}__}}`, value);
+  }
+
+  abstract render(...params: string[]): void;
+}
