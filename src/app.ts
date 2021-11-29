90e98c15b717ae5643dae8fc3517d76b78d66ba0
kyw <47565280+free-ko@users.noreply.github.com>
2021-11-29 21:00:17 +0900

    [FE] update : router.ts 개발

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import Router from "./core/router";
-import { NewsfeedView, NewsDetailView } from "./page";
+import { NewsFeedView, NewsDetailView } from "./page";
 import { Store } from "./store";
 
 const store = new Store();
 
 const router: Router = new Router();
-const newsFeedView = new NewsfeedView("root", store);
+const newsFeedView = new NewsFeedView("root", store);
 const newsDetailView = new NewsDetailView("root", store);
 
 router.setDefaultPage(newsFeedView);
+
 router.addRoutePath("/page/", newsFeedView, /page\/(\d+)/);
 router.addRoutePath("/show/", newsDetailView, /show\/(\d+)/);
